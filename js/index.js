9cacaedb2601017ab2c223fb21b90e1b1f730933
MYCOMPUTER\Owner <becevesti@gmail.com>
2022-12-18 17:34:14 +0900

    少し変更

--- js/index.js
+++ js/index.js
@@ -28,6 +28,6 @@ $(function () {
 window.addEventListener("scroll", function () {
   // ヘッダーを変数の中に格納する
   const header = document.querySelector(".header .l-S01");
-  // 100px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
+  // 500px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
   header.classList.toggle("scroll-nav", window.scrollY > 500);
 });
