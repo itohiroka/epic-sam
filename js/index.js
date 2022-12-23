939154e6748d70b53badfbe268d72ddeb2bbb65f
kawata <gonshitawaka@gmail.com>
2022-12-23 14:08:29 +0900

    index.jsのコード整理

--- js/index.js
+++ js/index.js
@@ -1,50 +1,45 @@
-$(function () {
-  // Drawer Menu
-  $(".js-button-hamburger").click(function () {
-    $("body").toggleClass("is-active-drawer");
-
-    if ($(this).attr("aria-expanded") == "false") {
-      $(this).attr("aria-expanded", true);
-    } else {
-      $(this).attr("aria-expanded", false);
-    }
-  });
-});
-
-$(function () {
-  var headerHeight = $(".header .l-S01").outerHeight(),
-    startPos = 0;
-  $(window).on("load scroll", function () {
-    var scrollPos = $(this).scrollTop();
-    if (scrollPos > startPos && scrollPos > headerHeight) {
-      $(".header .l-S01").css("top", "-" + headerHeight + "px");
-    } else {
-      $(".header .l-S01").css("top", "0");
-    }
-    startPos = scrollPos;
-  });
-});
-
-window.addEventListener("scroll", function () {
-  // ヘッダーを変数の中に格納する
-  const header = document.querySelector(".header .l-S01");
-  // 800px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
-  header.classList.toggle("scroll-nav", window.scrollY > 800);
-  fadeAnime();
-});
-
-$(window).on("load", function () {
-  fadeAnime();
-});
-
 function fadeAnime() {
   $(".fadeUpTrigger").each(function () {
-    var elemPos = $(this).offset().top;
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
+    const elemPos = $(this).offset().top;
+    const scroll = $(window).scrollTop();
+    const windowHeight = $(window).height();
 
     if (scroll >= elemPos - windowHeight) {
       $(this).addClass("fadeUp");
     }
   });
 }
+
+function changeBgColor() {
+  const header = document.querySelector(".header .l-S01");
+  header.classList.toggle("scroll-nav", window.scrollY > 800);
+}
+
+function headerAnime() {
+  const scrollPos = $(this).scrollTop();
+  if (scrollPos > startPos && scrollPos > headerHeight) {
+    $(".header .l-S01").css("top", "-" + headerHeight + "px");
+  } else {
+    $(".header .l-S01").css("top", "0");
+  }
+  startPos = scrollPos;
+}
+
+const headerHeight = $(".header .l-S01").outerHeight();
+let startPos = 0;
+
+$(window).on("scroll load", function () {
+  changeBgColor();
+  fadeAnime();
+  headerAnime();
+});
+
+$(".js-button-hamburger").click(function () {
+  $("body").toggleClass("is-active-drawer");
+
+  if ($(this).attr("aria-expanded") == "false") {
+    $(this).attr("aria-expanded", true);
+  } else {
+    $(this).attr("aria-expanded", false);
+  }
+});
